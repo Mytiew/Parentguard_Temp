57a782fb10388a3a224d0434a28b62ae2d1ce9c6
Tear <tup.kon@student.mahidol.edu>
2020-11-05 05:06:44 -0800

    fix bug where paraphrase feature did not work

--- content.js
+++ content.js
@@ -4,240 +4,221 @@ var isAdviced;
 var isAccessed;
 var isUnliked;
 likedpage = [];
-all=[];
+all = [];
 
 window.onload = likedPage();
-function likedPage(){
-    if(window.location.href == 'https://www.facebook.com/pages/?category=liked'){
-        var templistpage = document.getElementsByClassName("uiList uiCollapsedList uiCollapsedListHidden _7ywh _509- _4ki")
-        var listpageid = templistpage[0].id;
-        var idforlistpage = String(listpageid)
-        var listLikedPages = document.getElementById(idforlistpage)
-        var items = listLikedPages.getElementsByTagName("li")
-        
-        for(var i = 0; i < items.length; i++){
-            var item = items[i].getElementsByTagName('a')
-            if(item[1] == undefined) continue;
-            var str = item[1].getAttribute("href").split('/')
-            var page = str[3]
+function likedPage() {
+  if (
+    window.location.href == "https://www.facebook.com/pages/?category=liked"
+  ) {
+    var templistpage = document.getElementsByClassName(
+      "uiList uiCollapsedList uiCollapsedListHidden _7ywh _509- _4ki"
+    );
+    var listpageid = templistpage[0].id;
+    var idforlistpage = String(listpageid);
+    var listLikedPages = document.getElementById(idforlistpage);
+    var items = listLikedPages.getElementsByTagName("li");
 
-            var str1 = item[1].getElementsByTagName('span')
-            var pagename = str1[0].innerText
+    for (var i = 0; i < items.length; i++) {
+      var item = items[i].getElementsByTagName("a");
+      if (item[1] == undefined) continue;
+      var str = item[1].getAttribute("href").split("/");
+      var page = str[3];
 
-            var str2 = item[0].getElementsByTagName('img')
-            var img = str2[0].src
-            
-            var score = Math.floor(Math.random() * 85) + 15
-            
-            likedpage.push([page,pagename,img,score])
-            
+      var str1 = item[1].getElementsByTagName("span");
+      var pagename = str1[0].innerText;
 
-        }
-        chrome.runtime.sendMessage({content: likedpage, type: "monitor"});
-        // console.log('sent')
+      var str2 = item[0].getElementsByTagName("img");
+      var img = str2[0].src;
+
+      var score = Math.floor(Math.random() * 85) + 15;
+
+      likedpage.push([page, pagename, img, score]);
     }
+    chrome.runtime.sendMessage({ content: likedpage, type: "monitor" });
+  }
 }
 
-$(window).scroll(function() {
-	  	var scroll = $(window).scrollTop();
-	  if (scroll > position) {
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+  if (scroll > position) {
+    posts = document.querySelectorAll('div[data-ad-comet-preview="message"]');
+    comments = document.querySelectorAll(
+      "div.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.c1et5uql"
+    );
 
-		posts = document.querySelectorAll('div[data-ad-comet-preview="message"]')
-		comments = document.querySelectorAll('div.kvgmc6g5.cxmmr5t8.oygrvhab.hcukyx3x.c1et5uql')
-	    
-        console.log(posts)
-        console.log(`comments: ${comments}`)
-		for (var i = 0; i < posts.length; i++) {
-            if(!all.includes(posts[i])){
-                all.push(posts[i])
-                req(posts[i])
-            }
-		}
+    // console.log(posts)
+    // console.log(comments)
 
-		for (var i = 0; i < comments.length; i++) {
-            if(!all.includes(comments[i])){
-                all.push(comments[i])
-                req(comments[i])
-            }
-			
-		}
+    for (var i = 0; i < posts.length; i++) {
+      if (!all.includes(posts[i])) {
+        all.push(posts[i]);
+        req(posts[i]);
+      }
+    }
 
-	    position = scroll + 1500;
-	  
+    for (var i = 0; i < comments.length; i++) {
+      if (!all.includes(comments[i])) {
+        all.push(comments[i]);
+        req(comments[i]);
+      }
+    }
+
+    position = scroll + 1500;
   }
-  
 });
 
-
-function req(el){
-	
-    currentURL = window.location.href;
-    console.log('sdsdsdsdd  ')
-    if(preventWay == undefined) preventWay = "censor";
-    classify(el, preventWay, currentURL);
-    
-    
+function req(el) {
+  currentURL = window.location.href;
+  if (preventWay == undefined) preventWay = "censor";
+  classify(el, preventWay, currentURL);
 }
-function classify(el, preventWay,url){
-    const xxx = 'http://127.0.0.1:5000'
-    // const url = 'http://dacf9135c963.ngrok.io'
-    console.log('text',el.innerText)
-    console.log(preventWay, )
 
-    if(preventWay == "paraphrase"){
-        console.log('parparparp')
+function classify(el, preventWay, url) {
+  const api_url = "http://127.0.0.1:5000";
+  if (preventWay == "paraphrase") {
+    $.post(`${api_url}/para`, { javascript_data: el.innerText }, function (
+      data
+    ) {
+      el.innerText = data;
+      //  el.style.backgroundColor = 'red'
+    });
+  }
 
-    $.post(`${xxx}/para`, {javascript_data: el.innerText}, 
-            function( data ){
-console.log(data)
-         el.innerText = data
-         el.style.backgroundColor = 'red'
-        
-     })
+  $.post(
+    api_url,
+    {
+      javascript_data: el.innerText,
+    },
+    function (data) {
+      if (data) {
+        if (preventWay == "censor") {
+          el.style.backgroundColor = "transparent";
+          el.style.textShadow = "0 0 5px rgba(0,0,0,0.5)";
+          al = el.querySelectorAll("*");
+          for (var i = 0; i < al.length; i++) {
+            al[i].style.color = "transparent";
+            al[i].style.backgroundColor = "transparent";
+            al[i].style.textShadow = "0 0 5px rgba(0,0,0,0.5)";
+          }
+        } else if (preventWay == "paraphrase") {
+          $.post(
+            `${api_url}/para`,
+            {
+              javascript_data: el.innerText,
+            },
+            function (data) {
+              el.innerText = data;
+              el.style.backgroundColor = "yellow";
+            }
+          );
+        }
 
+        if (isAdviced == undefined) isAdviced = true;
+        if (isAdviced == true) adviceFeature(url);
+
+        if (isAccessed == undefined) isAccessed = true;
+        if (isAccessed == true) TooMuchAccessFeature(url);
+
+        if (isUnliked == undefined) isUnliked = true;
+        if (isUnliked == true) UnlikePageFeature(url);
+      }
     }
-
-    // if(preventWay == "censor"){
-
-    //     $.post(`${xxx}/para`, {
-    //          javascript_data: el.innerText}, 
-    //             function( data ){
-    //          el.innerText = data
-    //          el.style.backgroundColor = 'yellow'
-            
-    //      })
-    
-    //     }
-
-    $.post( xxx, {
-	    javascript_data: el.innerText}, 
-	    function( data ) {
-            if(data){
-                if(preventWay == "censor"){
-                    console.log(el.innerText)
-                    el.style.backgroundColor = 'transparent'
-                    el.style.textShadow = '0 0 5px rgba(0,0,0,0.5)' 
-                    al = el.querySelectorAll('*')
-                    for (var i = 0; i < al.length; i++) {
-                        al[i].style.color = 'transparent'
-                        al[i].style.backgroundColor = 'transparent'
-                        al[i].style.textShadow = '0 0 5px rgba(0,0,0,0.5)'
-                    }
-             }else if(preventWay == "paraphrase"){
-                console.log('parparparp')
-                $.post(`${xxx}/para`, {
-                    javascript_data: el.innerText}, 
-                       function( data ){
-                    el.innerText = data
-                    el.style.backgroundColor = 'yellow'
-                    
-                })
-                
-            }
-                
-                  if(isAdviced == undefined) isAdviced = true;
-                    if(isAdviced == true)
-                        adviceFeature(url);
-
-                    if(isAccessed == undefined) isAccessed = true;
-                    if(isAccessed == true)
-                        TooMuchAccessFeature(url);
-
-                    if(isUnliked == undefined) isUnliked = true;
-                    if(isUnliked == true)
-                        UnlikePageFeature(url);
-            }
-	});
+  );
 }
 
-chrome.extension.onMessage.addListener(function(message,sender,sendResponse){
+chrome.extension.onMessage.addListener(function (
+  message,
+  sender,
+  sendResponse
+) {
   //This is where the stuff you want from the background page will be
-  preventWay = message.stuff
-  isAdviced = message.stuff1
-  isAccessed = message.stuff2
-  isUnliked = message.stuff3
+  preventWay = message.stuff;
+  isAdviced = message.stuff1;
+  isAccessed = message.stuff2;
+  isUnliked = message.stuff3;
 });
 
 countPostPage = 0;
 countPostTimeline = 0;
-function adviceFeature(currentUrl){
-    if(currentUrl != 'https://www.facebook.com/'){
-        countPostPage++;
-        if(countPostPage == 3){
-            url_img = "https://pbs.twimg.com/media/DeSW1YzUwAA7TPx.jpg";
-            img = '<img src="'+url_img+'">';
-            text = "<h1>เพจนี้ไม่เหมาะกับหนูหรอกลูก ออกมาเถอะนะ ถือว่าแม่ขอ!!!</h1>"
-            popup = window.open();
-            popup.document.write(text+img); 
-        }
+function adviceFeature(currentUrl) {
+  if (currentUrl != "https://www.facebook.com/") {
+    countPostPage++;
+    if (countPostPage == 3) {
+      url_img = "https://pbs.twimg.com/media/DeSW1YzUwAA7TPx.jpg";
+      img = '<img src="' + url_img + '">';
+      text = "<h1>เพจนี้ไม่เหมาะกับหนูหรอกลูก ออกมาเถอะนะ ถือว่าแม่ขอ!!!</h1>";
+      popup = window.open();
+      popup.document.write(text + img);
     }
-    else {
-        countPostTimeline++;
-        if(countPostTimeline == 3){
-           url_img = "https://www.amarinbabyandkids.com/app/uploads/2017/06/%E0%B8%94%E0%B8%B8.jpg";
-            img = '<img src="'+url_img+'">';
-            text = "<h1>อ่านอะไรอยู่คะลูก แม่เห็นนะ เตือนแล้วนะคะลูกรัก</h1>" 
-            popup = window.open();
-            popup.document.write(text+img); 
-        }      
+  } else {
+    countPostTimeline++;
+    if (countPostTimeline == 3) {
+      url_img =
+        "https://www.amarinbabyandkids.com/app/uploads/2017/06/%E0%B8%94%E0%B8%B8.jpg";
+      img = '<img src="' + url_img + '">';
+      text = "<h1>อ่านอะไรอยู่คะลูก แม่เห็นนะ เตือนแล้วนะคะลูกรัก</h1>";
+      popup = window.open();
+      popup.document.write(text + img);
     }
-       
-    }
-        
+  }
+}
 
 countForNotifyPage = 0;
 countForNotifyTimeline = 0;
-function TooMuchAccessFeature(currentUrl){
-       if(currentUrl != 'https://www.facebook.com/'){
-           countForNotifyPage++;
-           if(countForNotifyPage == 6){
-               chrome.runtime.sendMessage({content: currentUrl, type: "m1"});
-           }
-       }
-       else{
-           countForNotifyTimeline++;
-           if(countForNotifyTimeline == 6){
-               chrome.runtime.sendMessage({content: currentUrl, type: "m2"});
-           }
-       }
+function TooMuchAccessFeature(currentUrl) {
+  if (currentUrl != "https://www.facebook.com/") {
+    countForNotifyPage++;
+    if (countForNotifyPage == 6) {
+      chrome.runtime.sendMessage({ content: currentUrl, type: "m1" });
+    }
+  } else {
+    countForNotifyTimeline++;
+    if (countForNotifyTimeline == 6) {
+      chrome.runtime.sendMessage({ content: currentUrl, type: "m2" });
+    }
+  }
 }
 
 countForUnlikePage = 0;
-function UnlikePageFeature(currentUrl){
-        countForUnlikePage++;
-        if(countForUnlikePage == 10){
-        if(currentUrl != 'https://www.facebook.com/'){
-            setTimeout(function() {  
-                hov = document.querySelector('a[data-testid="page_profile_liked_button_test_id"]');
-                hov.click(); 
-            }, 100);
-            setTimeout(function(){  
-                leave_group = document.querySelector('a[data-testid="page_unlike_item_test_id"]');
-                leave_group.click(); 
-            }, 300);
-            setTimeout(function(){  
-                close = document.querySelector('a[data-testid="dialog_title_close_button"]');
-                close.click(); 
-            },1000);
-            setTimeout(function(){  
-                close = document.querySelector('a[data-testid="dialog_title_close_button"]');
-                close.click(); 
-            },1100);
-            setTimeout(function(){  
-                close = document.querySelector('a[data-testid="dialog_title_close_button"]');
-                close.click(); 
-            },1200);
-            
-            setTimeout(function(){
-                window.location.replace('https://www.facebook.com/')
-            },100);
-            
-        }
-       
-            
+function UnlikePageFeature(currentUrl) {
+  countForUnlikePage++;
+  if (countForUnlikePage == 10) {
+    if (currentUrl != "https://www.facebook.com/") {
+      setTimeout(function () {
+        hov = document.querySelector(
+          'a[data-testid="page_profile_liked_button_test_id"]'
+        );
+        hov.click();
+      }, 100);
+      setTimeout(function () {
+        leave_group = document.querySelector(
+          'a[data-testid="page_unlike_item_test_id"]'
+        );
+        leave_group.click();
+      }, 300);
+      setTimeout(function () {
+        close = document.querySelector(
+          'a[data-testid="dialog_title_close_button"]'
+        );
+        close.click();
+      }, 1000);
+      setTimeout(function () {
+        close = document.querySelector(
+          'a[data-testid="dialog_title_close_button"]'
+        );
+        close.click();
+      }, 1100);
+      setTimeout(function () {
+        close = document.querySelector(
+          'a[data-testid="dialog_title_close_button"]'
+        );
+        close.click();
+      }, 1200);
+
+      setTimeout(function () {
+        window.location.replace("https://www.facebook.com/");
+      }, 100);
     }
+  }
 }
-
-
-
-                         
--- page_monitor.js
+++ page_monitor.js
@@ -1,145 +1,225 @@
-a=[];
-b=[];
+a = [];
+b = [];
 page = [];
-page = [{'name':'nongngneverdie','score':87,'pagename':'น้องง','image':'<img src="nongg.jpg" width="50">'}]
-page.push({'name':'khaopodnews','score':45,'pagename':'ข่าวปด','image':'<img src="khaopod.jpg" width="50">'})
-page.push({'name':'quoteV2','score':65,'pagename':'วิวาทะV2','image':'<img src="quotev2.jpg" width="50">'})
-page.push({'name':'daghee','score':78,'pagename':'แดก','image':'<img src="daghee.jpg" width="50">'})
-page.push({'name':'icepadie','score':15,'pagename':'','image':'<img src="padie.jpg" width="50">'})
-page.push({'name':'MightyCaster','score':30,'pagename':'HeartRocker','image':'<img src="rocker.jpg" width="50">'})
+page = [
+  {
+    name: "nongngneverdie",
+    score: 87,
+    pagename: "น้องง",
+    image: '<img src="nongg.jpg" width="50">',
+  },
+];
+page.push({
+  name: "khaopodnews",
+  score: 45,
+  pagename: "ข่าวปด",
+  image: '<img src="khaopod.jpg" width="50">',
+});
+page.push({
+  name: "quoteV2",
+  score: 65,
+  pagename: "วิวาทะV2",
+  image: '<img src="quotev2.jpg" width="50">',
+});
+page.push({
+  name: "daghee",
+  score: 78,
+  pagename: "แดก",
+  image: '<img src="daghee.jpg" width="50">',
+});
+page.push({
+  name: "icepadie",
+  score: 15,
+  pagename: "",
+  image: '<img src="padie.jpg" width="50">',
+});
+page.push({
+  name: "MightyCaster",
+  score: 30,
+  pagename: "HeartRocker",
+  image: '<img src="rocker.jpg" width="50">',
+});
 
 checkexistingpage = [];
 
 document.getElementById("allPage").innerHTML = listAllPage();
 
-function addToLikedPage(allpage){
-    for(var w = 0; w < allpage.length;w++){
-        if(!checkexistingpage.includes(allpage[w][0])){
-            page.push({'name':allpage[w][0],'score':allpage[w][3],'pagename':allpage[w][1],'image':'<img src="'+allpage[w][2]+'" width="50">'})
-            checkexistingpage.push(allpage[w][0])
-        }
-        
+function addToLikedPage(allpage) {
+  for (var w = 0; w < allpage.length; w++) {
+    if (!checkexistingpage.includes(allpage[w][0])) {
+      page.push({
+        name: allpage[w][0],
+        score: allpage[w][3],
+        pagename: allpage[w][1],
+        image: '<img src="' + allpage[w][2] + '" width="50">',
+      });
+      checkexistingpage.push(allpage[w][0]);
     }
-    console.log(page)
-    for(var j = 6; j < page.length; j++){
-        
-        if(page[j].score >= 45){
-           var t = document.getElementById("customers");
-            var r = t.insertRow(t.rows.length);
-            r.id = page[j].name
-            var c1 = r.insertCell(0);
-            var c2 = r.insertCell(1);
-            var c3 = r.insertCell(2);
-            var c4 = r.insertCell(3);
-            c1.innerHTML = page[j].image+" "+page[j].name
-            c2.innerHTML = page[j].pagename 
-            c3.innerHTML = page[j].score
-            c4.innerHTML = levelOfAbusive(page[j].score)
-            document.getElementById(r.id).addEventListener("click", linkToFacebookPage(page[j].name),false)  
-        }
-        else{
-            var t = document.getElementById("customers2");
-            var r = t.insertRow(t.rows.length);
-            r.id = page[j].name
-            var c1 = r.insertCell(0);
-            var c2 = r.insertCell(1);
-            var c3 = r.insertCell(2);
-            var c4 = r.insertCell(3);
-            c1.innerHTML = page[j].image+" "+page[j].name
-            c2.innerHTML = page[j].pagename 
-            c3.innerHTML = page[j].score
-            c4.innerHTML = levelOfAbusive(page[j].score)
-            document.getElementById(r.id).addEventListener("click", linkToFacebookPage(page[j].name),false)
-        } 
-    }
-}
-function levelOfAbusive(percentage){
-    if(percentage < 45)
-        return '<div id= "rectangle2" style="height:24px;width:100%"><div id="rectangleGreen" style="height:24px;width:'+percentage+'%"></div></div>'
-    else if(percentage > 45 && percentage < 70)
-        return '<div id= "rectangle2" style="height:24px;width:100%"><div id="rectangleYellow" style="height:24px;width:'+percentage+'%"></div></div>'
-    else
-        return '<div id= "rectangle2" style="height:24px;width:100%"><div id="rectangleRed" style="height:24px;width:'+percentage+'%"></div></div>'
-}
-
-function listAllPage(){
-    a.push("<table id='customers' class='display' style='width:60%'; align='center'><thead><tr bgcolor='#FF0000'><th width='25%'>Pages</th><th width='20%'>Page's name</th><th width='25%'>Abusive scores in numeric</th><th width='50%'>Abusive  scores in bar chart</th></tr></thead><tbody>")
-    b.push("<table id='customers2' class='display' style='width:60%'; align='center'><thead><tr bgcolor='#5ED4FB'><th width='25%'>Pages</th><th width='20%'>Page's name</th><th width='25%'>Abusive scores in numeric</th><th width='50%'>Abusive  scores in bar chart</th></tr></thead><tbody>")
-    
-    var i;
-    for(i=0;i<page.length;i++){ 
-        if(page[i].score>=45){
-            a.push("<tr id='"+page[i].name+"'><th>"+page[i].image+" "+page[i].name+"</th><th>"+page[i].pagename+"</th><th>"+page[i].score+"<th>"+levelOfAbusive(page[i].score)+"</th></tr>")
-        }
-        else{
-            b.push("<tr id='"+page[i].name+"'><th>"+page[i].image+" "+page[i].name+"</th><th>"+page[i].pagename+"</th><th>"+page[i].score+"<th>"+levelOfAbusive(page[i].score)+"</th></tr>")
-        }
-    }
-    a.push("</tbody><table>")
-    b.push("</tbody><table>")
-    return a+"<br><h1 align='center' style='font-family: Rockwell; color:darkslategray'>Good Followed Pages Report</h1>"+b;
-}
-
-function linkToFacebookPage(pageName){
-    return function(){
-        myFunction(pageName)
-    }
-}
-
-for(var i=0;i<page.length;i++){
-    document.getElementById(page[i].name).addEventListener("click", linkToFacebookPage(page[i].name),false)
-}
-
-function myFunction(pageName){
-  window.open('https://www.facebook.com/'+pageName);
-}
-document.getElementById('customers').addEventListener('click', function(){
-    $('#customers').DataTable({
-        order:[2,'des'],
-        destroy: true,
-        searching: false,
-        paging: false,
-        info: false
-        
-    });
-})
-
-document.getElementById('customers2').addEventListener('click', function(){
-    $('#customers2').DataTable({
-        order:[2,'asc'],
-        destroy: true,
-        searching: false,
-        paging: false,
-        info: false
-        
-    });
-})
-
-chrome.runtime.onMessage.addListener(function(message) {
-  if(message.type == "m1" || message.type == "m2") {
-    sendMail(message.content);
   }
-  else if(message.type == "monitor"){
-      console.log("liked pages are here!")
-      console.log(message.content)
-      addToLikedPage(message.content);
-  } 
-})
+  for (var j = 6; j < page.length; j++) {
+    if (page[j].score >= 45) {
+      var t = document.getElementById("customers");
+      var r = t.insertRow(t.rows.length);
+      r.id = page[j].name;
+      var c1 = r.insertCell(0);
+      var c2 = r.insertCell(1);
+      var c3 = r.insertCell(2);
+      var c4 = r.insertCell(3);
+      c1.innerHTML = page[j].image + " " + page[j].name;
+      c2.innerHTML = page[j].pagename;
+      c3.innerHTML = page[j].score;
+      c4.innerHTML = levelOfAbusive(page[j].score);
+      document
+        .getElementById(r.id)
+        .addEventListener("click", linkToFacebookPage(page[j].name), false);
+    } else {
+      var t = document.getElementById("customers2");
+      var r = t.insertRow(t.rows.length);
+      r.id = page[j].name;
+      var c1 = r.insertCell(0);
+      var c2 = r.insertCell(1);
+      var c3 = r.insertCell(2);
+      var c4 = r.insertCell(3);
+      c1.innerHTML = page[j].image + " " + page[j].name;
+      c2.innerHTML = page[j].pagename;
+      c3.innerHTML = page[j].score;
+      c4.innerHTML = levelOfAbusive(page[j].score);
+      document
+        .getElementById(r.id)
+        .addEventListener("click", linkToFacebookPage(page[j].name), false);
+    }
+  }
+}
+function levelOfAbusive(percentage) {
+  if (percentage < 45)
+    return (
+      '<div id= "rectangle2" style="height:24px;width:100%"><div id="rectangleGreen" style="height:24px;width:' +
+      percentage +
+      '%"></div></div>'
+    );
+  else if (percentage > 45 && percentage < 70)
+    return (
+      '<div id= "rectangle2" style="height:24px;width:100%"><div id="rectangleYellow" style="height:24px;width:' +
+      percentage +
+      '%"></div></div>'
+    );
+  else
+    return (
+      '<div id= "rectangle2" style="height:24px;width:100%"><div id="rectangleRed" style="height:24px;width:' +
+      percentage +
+      '%"></div></div>'
+    );
+}
+
+function listAllPage() {
+  a.push(
+    "<table id='customers' class='display' style='width:60%'; align='center'><thead><tr bgcolor='#FF0000'><th width='25%'>Pages</th><th width='20%'>Page's name</th><th width='25%'>Abusive scores in numeric</th><th width='50%'>Abusive  scores in bar chart</th></tr></thead><tbody>"
+  );
+  b.push(
+    "<table id='customers2' class='display' style='width:60%'; align='center'><thead><tr bgcolor='#5ED4FB'><th width='25%'>Pages</th><th width='20%'>Page's name</th><th width='25%'>Abusive scores in numeric</th><th width='50%'>Abusive  scores in bar chart</th></tr></thead><tbody>"
+  );
+
+  var i;
+  for (i = 0; i < page.length; i++) {
+    if (page[i].score >= 45) {
+      a.push(
+        "<tr id='" +
+          page[i].name +
+          "'><th>" +
+          page[i].image +
+          " " +
+          page[i].name +
+          "</th><th>" +
+          page[i].pagename +
+          "</th><th>" +
+          page[i].score +
+          "<th>" +
+          levelOfAbusive(page[i].score) +
+          "</th></tr>"
+      );
+    } else {
+      b.push(
+        "<tr id='" +
+          page[i].name +
+          "'><th>" +
+          page[i].image +
+          " " +
+          page[i].name +
+          "</th><th>" +
+          page[i].pagename +
+          "</th><th>" +
+          page[i].score +
+          "<th>" +
+          levelOfAbusive(page[i].score) +
+          "</th></tr>"
+      );
+    }
+  }
+  a.push("</tbody><table>");
+  b.push("</tbody><table>");
+  return (
+    a +
+    "<br><h1 align='center' style='font-family: Rockwell; color:darkslategray'>Good Followed Pages Report</h1>" +
+    b
+  );
+}
+
+function linkToFacebookPage(pageName) {
+  return function () {
+    myFunction(pageName);
+  };
+}
+
+for (var i = 0; i < page.length; i++) {
+  document
+    .getElementById(page[i].name)
+    .addEventListener("click", linkToFacebookPage(page[i].name), false);
+}
+
+function myFunction(pageName) {
+  window.open("https://www.facebook.com/" + pageName);
+}
+document.getElementById("customers").addEventListener("click", function () {
+  $("#customers").DataTable({
+    order: [2, "des"],
+    destroy: true,
+    searching: false,
+    paging: false,
+    info: false,
+  });
+});
+
+document.getElementById("customers2").addEventListener("click", function () {
+  $("#customers2").DataTable({
+    order: [2, "asc"],
+    destroy: true,
+    searching: false,
+    paging: false,
+    info: false,
+  });
+});
+
+chrome.runtime.onMessage.addListener(function (message) {
+  if (message.type == "m1" || message.type == "m2") {
+    sendMail(message.content);
+  } else if (message.type == "monitor") {
+    addToLikedPage(message.content);
+  }
+});
 
 function sendMail(url) {
-    if(url != 'https://www.facebook.com/'){
-        body = "ลูกคุณกำลังเข้าถึงข้อความที่ไม่เหมาะสมผ่านเว็บเพจนี้ "+url
-    }
-    else{
-        body = "ลูกคุณกำลังเข้าถึงข้อความที่ไม่เหมาะสมในขณะใช้งานเฟสบุ๊ค"
-    }
- Email.send({
-				Host: "smtp.gmail.com",
-				Username : "manisa.sar@gmail.com",
-				Password : "0616324963",
-				To : 'manisa.sar@gmail.com',
-				From : "manisa.sar@gmail.com",
-				Subject : "Caution!!",
-				Body : body,
-			});
+  if (url != "https://www.facebook.com/") {
+    body = "ลูกคุณกำลังเข้าถึงข้อความที่ไม่เหมาะสมผ่านเว็บเพจนี้ " + url;
+  } else {
+    body = "ลูกคุณกำลังเข้าถึงข้อความที่ไม่เหมาะสมในขณะใช้งานเฟสบุ๊ค";
+  }
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "manisa.sar@gmail.com",
+    Password: "0616324963",
+    To: "manisa.sar@gmail.com",
+    From: "manisa.sar@gmail.com",
+    Subject: "Caution!!",
+    Body: body,
+  });
 }
